eb6f8812cd40832e81db31422316fd5af9dde252
levias-dev <levias-dev@growtech.co.jp>
2021-03-30 18:18:06 +0900

    add

---
+++ app/app/src/main/java/edu/hytc/disflowers/HomePageController.java
@@ -0,0 +1,106 @@
+package edu.hytc.disflowers;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HomePageController extends AppCompatActivity {
+
+    protected static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    protected static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
+
+    private Bitmap bitmap;
+
+    //拍照按钮
+    private Button takePicture;
+
+    //相册按钮
+    private Button myAlbum;
+
+    //图片显示
+    private ImageView imageView;
+
+    public HomePageController() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_controller);
+
+        //初始化按钮
+        takePicture = findViewById(R.id.takePicture);
+        myAlbum = findViewById(R.id.myAlbum);
+        imageView = findViewById(R.id.showImage);
+
+
+        //设置监听事件 -- 拍照
+        takePicture.setOnClickListener(this::takePicturefunction);
+
+        //设置监听事件 -- 显示相册
+        myAlbum.setOnClickListener(this::showAblum);
+
+    }
+
+    /**
+     * 调用拍照功能
+     * 　　　１.调用系统相机
+    　*　　　２.拍照好了之后回掉
+     *      ３.跳转画面
+     * @param view
+     */
+    private void takePicturefunction(View view){
+
+        Toast.makeText(this, "调用相机", Toast.LENGTH_SHORT).show();
+        //１.调用系统相机
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /**
+     * 调用拍照功能
+     *      ２.拍照好了之后回掉  保存图片
+     *      ３.跳转画面
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Toast.makeText(this, "call back", Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    /**
+     *  显示相册
+     * @param view
+     */
+    private void showAblum(View view){
+
+    }
+
+
+}
---
+++ app/app/src/main/java/edu/hytc/disflowers/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.hytc.disflowers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 跳转到Home画面
+     * @param view
+     */
+    public void move2HomePage(View view){
+        Intent intent = new Intent(this, HomePageController.class);
+        startActivity(intent);
+    }
+
+}
