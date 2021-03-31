518b828de22a211b0125d603a5ce711965e78e75
gucanlin <294403373@qq.com>
2021-03-31 23:49:16 +0900

    up

--- DisFlowers/app/src/main/java/edu/hytc/disflowers/DisResultController.java
+++ DisFlowers/app/src/main/java/edu/hytc/disflowers/DisResultController.java
@@ -1,13 +1,31 @@
 package edu.hytc.disflowers;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DisResultController extends AppCompatActivity {
+
+    //処理バタンを押して
+    private Button seeLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dis_result_controller);
+
+        seeLog = findViewById(R.id.seeLog);
+        seeLog.setOnClickListener(this::processSeeLog);
+    }
+
+    private void processSeeLog(View view){
+
+
+        //得到
+        Intent intent = new Intent(this, HistoryList.class);
+        startActivity(intent);
     }
 }
